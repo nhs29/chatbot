47cee20971262e39a59227948ba2f5a074e3bde0
nhs29 <156171684+nhs29@users.noreply.github.com>
2024-04-16 22:35:38 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,25 +6,19 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain import PromptTemplate
 
-
 st.set_page_config(page_title="Document Genie", layout="wide")
-st.markdown("""## Document Genie: Get instant insights from your Documents...""")
+st.markdown("""## History Chatbot""")
 
 vectorstore = FAISS.load_local("faiss_store (4)", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
 retriever = vectorstore.as_retriever()
 
 template = """You are a helpful assistant.
-Use the following pieces of context, if needed, to answer the input at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-\n\nContext: {context}
-\n\nChat history: {chat_history}
-New question: {question}
-Helpful answer:"""
+    Use the following pieces of context, if needed, to answer the input at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    \n\nContext: {context}
+    \n\nChat history: {chat_history}
+    New question: {question}
+    Helpful answer:"""
 prompt = PromptTemplate.from_template(template)
-
-llm = OpenAI(temperature=0.5)
-
-memory = ConversationBufferMemory(llm=llm, memory_key="chat_history", output_key='answer', return_messages=True)
-chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory, combine_docs_chain_kwargs={"prompt": prompt}, verbose=True, rephrase_question=False)
 
 
 def ask(question):
@@ -36,6 +30,11 @@ if __name__ == "__main__":
     key = st.text_input("API key")
     if key:
         os.environ["OPENAI_API_KEY"] = key
-        question = st.text_input("Ask a question")
-        if question:
-            ask(question)
+        llm = OpenAI(temperature=0.5)
+
+        memory = ConversationBufferMemory(llm=llm, memory_key="chat_history", output_key='answer', return_messages=True)
+        chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory, combine_docs_chain_kwargs={"prompt": prompt}, verbose=True, rephrase_question=False)
+
+    question = st.text_input("Ask a question")
+    if question:
+        ask(question)
