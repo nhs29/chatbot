e0a26c4f39285a43e08062db0bfe12d6cd93a045
nhs29 <156171684+nhs29@users.noreply.github.com>
2024-04-16 22:18:28 +0800

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,40 @@
+import os
+import streamlit as st
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain import PromptTemplate
+
+
+st.set_page_config(page_title="Document Genie", layout="wide")
+st.markdown("""## Document Genie: Get instant insights from your Documents...""")
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+vectorstore = FAISS.load_local("faiss_store (4)", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
+retriever = vectorstore.as_retriever()
+
+template = """You are a helpful assistant.
+Use the following pieces of context, if needed, to answer the input at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+\n\nContext: {context}
+\n\nChat history: {chat_history}
+New question: {question}
+Helpful answer:"""
+prompt = PromptTemplate.from_template(template)
+
+llm = OpenAI(temperature=0.5)
+
+memory = ConversationBufferMemory(llm=llm, memory_key="chat_history", output_key='answer', return_messages=True)
+chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory, combine_docs_chain_kwargs={"prompt": prompt}, verbose=True, rephrase_question=False)
+
+
+def ask(question):
+    response = chain({"question": question})
+    st.write(response["answer"].strip())
+
+
+if __name__ == "__main__":
+    question = st.text_input("Ask a question")
+    if question:
+        ask(question)
